7c74016a7e585791c37978722872935d6d6bb4a4
zhangqi16 <1318697650@qq.com>
2021-03-27 22:07:16 +0800

    部署上线BS层

--- jdbs-web/src/main/java/com/coms/jd/WebApplication.java
+++ jdbs-web/src/main/java/com/coms/jd/WebApplication.java
@@ -3,18 +3,11 @@ package com.coms.jd;
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = {"com.coms.jd"})
 @EnableDubbo
-public class WebApplication extends SpringBootServletInitializer {
+public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
-    }
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        // 注意这里要指向原先用main方法执行的Application启动类
-        return builder.sources(WebApplication.class);
     }
 }
